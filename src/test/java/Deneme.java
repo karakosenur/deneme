c0fdafda4bed034a553af06a79b8d68f6e0f50c1
karakosenur <karakosenur@gmail.com>
2023-04-10 10:24:38 +0300

    deneme

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,5 +1,8 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("deneme");
+
+        //hocam bu bilgisayar abimindi. altta diyor ya denied to nunix123 diye
+        // bi yerden onun github baglaniyor sanki ama nereden anlamiyorum
     }
 }
